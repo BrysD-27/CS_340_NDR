8832f4788e684de70382ae14c0a60522c09f902b
Bryson <brysondavis71@yahoo.com>
2025-05-06 23:41:02 -0500

    require fields add routing

--- app.py
+++ app.py
@@ -6,5 +6,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/supplies')
+def supplies():
+    return render_template('supplies.html')
+
+@app.route('/drivers')
+def drivers():
+    return render_template('drivers.html')
+
+@app.route('/recipients')
+def recipients():
+    return render_template('recipients.html')
+
+@app.route('/deliveries')
+def deliveries():
+    return render_template('deliveries.html')
+
+@app.route('/deliveries-supplies')
+def deliveries_supplies():
+    return render_template('deliveries_supplies.html')
+
 if __name__ == '__main__':
-    app.run(port=4200, debug=True)
+    app.run(port=9313, debug=True)
