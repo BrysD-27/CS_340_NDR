6112eab9421f7a62a9a110a547d753306c2b3bfb
BrysD-27 <brysondavis65@gmail.com>
2025-05-23 23:17:59 -0500

    update sproc

--- PL.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE sp_delete_latest_driver()
-BEGIN
-    DELETE FROM Drivers
-    WHERE driverID = (
-        SELECT MAX(driverID) FROM Drivers
-    );
-END //
-DELIMITER ;
