e207a7a5a862ccf605295e2f260f823b6ea6c012
BrysD-27 <brysondavis65@gmail.com>
2025-06-01 16:08:22 -0500

    step 5

--- PL.sql
+++ PL.sql
@@ -1,10 +1,40 @@
-DELIMITER //
-DROP PROCEDURE  IF EXISTS sp_delete_latest_driver();
-CREATE PROCEDURE sp_delete_latest_driver()
+-- DeliveriesSupplies CUD Procedures
+
+DELIMITER $$
+
+-- CREATE procedure
+CREATE PROCEDURE sp_add_delivery_supply(
+    IN p_deliveryID INT,
+    IN p_supplyID INT,
+    IN p_supplyQuantity INT
+)
 BEGIN
-    DELETE FROM Drivers
-    WHERE driverID = (
-        SELECT MAX(driverID) FROM Drivers
-    );
-END //
+    INSERT INTO DeliveriesSupplies (deliveryID, supplyID, supplyQuantity)
+    VALUES (p_deliveryID, p_supplyID, p_supplyQuantity);
+END$$
+
+-- UPDATE procedure
+CREATE PROCEDURE sp_update_delivery_supply(
+    IN p_deliverySupplyID INT,
+    IN p_deliveryID INT,
+    IN p_supplyID INT,
+    IN p_supplyQuantity INT
+)
+BEGIN
+    UPDATE DeliveriesSupplies
+    SET deliveryID = p_deliveryID,
+        supplyID = p_supplyID,
+        supplyQuantity = p_supplyQuantity
+    WHERE deliverySupplyID = p_deliverySupplyID;
+END$$
+
+-- DELETE procedure
+CREATE PROCEDURE sp_delete_delivery_supply(
+    IN p_deliverySupplyID INT
+)
+BEGIN
+    DELETE FROM DeliveriesSupplies
+    WHERE deliverySupplyID = p_deliverySupplyID;
+END$$
+
 DELIMITER ;
